88efaa67952f9b43990cd3285a3f778009e28c18
ZCodingKid <ZCodingKid@gmail.com>
2023-02-09 16:02:25 -0500

    made it look good

--- notes.py
+++ notes.py
@@ -1,8 +1,9 @@
 #init
 from tkinter import *
+from PIL import Image, ImageTk
 
 window = Tk()
-window.geometry("900x900")
+window.geometry("400x400")
 window.title("To-Do List")
 window.resizable(False, False)
 notes = Listbox(window)
@@ -21,14 +22,14 @@ def get_box():
     notes.insert(1 + i, retrieve)
     i += 1
 def show_notes( ):
-    notes.pack() 
+    notes.place(x=107, y=150)
 def delete():
     db = notes.curselection()
 
     db = int(db[0])
     notes.delete(db )
 def hide_notes(): 
-    notes.pack_forget()
+    notes.place_forget()
 def openNewWindow():
 
      
@@ -49,21 +50,37 @@ def openNewWindow():
           text =str(f.read())).pack()
     f.close()
 #Files
-DELETE_IMG = PhotoImage(file='/Users/zaidr/Downloads/delete.png')
+# Show Image
+show_img = Image.open('/Users/zaidr/Downloads/show.png')
+show_img = show_img.resize((35, 35))
+SHOW_IMG = ImageTk.PhotoImage(show_img)
+# Hide Image
+hide_img = Image.open('/Users/zaidr/Downloads/hide.png')
+hide_img = hide_img.resize((35, 35))
+HIDE_IMG = ImageTk.PhotoImage(hide_img)
+# Add Image
+add_img = Image.open('/Users/zaidr/Downloads/plus.png')
+add_img = add_img.resize((35, 35))
+ADD_IMG = ImageTk.PhotoImage(add_img)
+# Delete Image
+delete_img = Image.open('/Users/zaidr/Downloads/del.png')
+delete_img = delete_img.resize((35, 35))
+DELETE_IMG = ImageTk.PhotoImage(delete_img)
 #Components
 delete = Button(window, text="Delete", command=delete, image=DELETE_IMG, borderwidth=0)
-delete.pack(pady=10)
-hide = Button(window, command=hide_notes, text="Hide Notes", font="Times")
+hide = Button(window, command=hide_notes, text="Hide Notes", font="Times", image=HIDE_IMG, borderwidth=0)
 ENTRY_BOX = Text(window, height="3", width="35", font="Times")
-ADD_NOTE = Button(window, command=get_box, text="Add Note", font="Times")  
-show_notes = Button(window, command=show_notes, text="Show Notes", font="Times")
+ADD_NOTE = Button(window, command=get_box, text="Add Note", font="Times", image=ADD_IMG)  
+show_notes = Button(window, command=show_notes, text="Show Notes", font="Times", image=SHOW_IMG)
 show_all = Button(window, text="Show All Notes", command=openNewWindow, font="Times")
 #Pack
-show_all.pack()
+show_all.pack(pady=5)
 ENTRY_BOX.pack()
-ADD_NOTE.pack()
-show_notes.pack()
-hide.pack()
+ADD_NOTE.place(x=100, y= 100)
+show_notes.place(x=150, y=100)
+hide.place(x=200, y=100)
+delete.place(x=250, y=100)
+
 
 
 #Mainloop
